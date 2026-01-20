2a0fd22d83cf81621443cfc4c59b443b385ccc39
phoenixlifemail-dev <phoenix.life.mail@gmail.com>
2026-01-21 00:34:17 +0530

    Add main JavaScript file

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*
+  main.js
+  ----------
+  This file is intentionally minimal.
+  Future features like navigation, language switch,
+  and interactivity will go here.
+*/
+
+console.log("Website loaded successfully");
